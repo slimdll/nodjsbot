0a92fceafbfc17c9754a9fb813e0b1f5100f4cdf
slimdll <37386769+slimdll@users.noreply.github.com>
2018-03-23 15:09:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const  TelegramBot  =  require ( 'node-telegram-bot-api' )
+const TelegramBot = require ('node-telegram-bot-api')
 const TOKEN = '537949933:AAGcSvlIr49Ikr0TS0POL3hvyn34scVqHZ4'
-const bot = new TelegramBot({
-  token: token,
-  polling: {limit: 50}
-})
+const bot = new TelegramBot('TOKEN', { polling: true})
+  
 
-bot.on(['/start', '/hello'], (msg) => {
-  if(msg.chat.type != 'supergroup')
-    return msg.reply.text("Sorry, I only work with supergroups")
-  console.log(msg)
-  msg.reply.text(msg.text.slice(msg.entities[0].length) || '<Untitled>')
-  .then( d => {
-    bot.pinChatMessage(d.result.chat.id, d.result.message_id)
-  })
+bot.on(['message', msg => {
+  bot.sendMessage(msg.chat.Id, 'Hello Heroku,')
 })
 
 bot.on(['/add', '/anclar'], require('./lib/cmd_add')(bot) )
